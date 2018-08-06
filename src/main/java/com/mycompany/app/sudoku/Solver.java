689b04929eb1bcc9787fd16f4abb880217478705
Jeff <sparklicorn@users.noreply.github.com>
2018-08-05 20:39:35 -0400

    Solver refactor

--- src/main/java/com/mycompany/app/sudoku/Solver.java
+++ src/main/java/com/mycompany/app/sudoku/Solver.java
@@ -6,16 +6,21 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.mycompany.app.sudoku.Board.*;
 
 /**
- * TODO WIP
+ * 
  * 
  * @author Jeff
  */
 public class Solver {
 	
+	private static interface SolutionFoundPolicy {
+		public boolean execute(Board b);
+	}
+
 	/**
 	 * Attempts to solve the given Sudoku board, returning the first
 	 * solution found, or null if no solution was found.
@@ -27,53 +32,12 @@ public class Solver {
 	 * @return A new ISudokuBoard representing the puzzle solution.
 	 */
 	public static Board solve(Board board) {
-		//This will be reused and repopulated by board.getCandidates(list)
-		//	to reduce overhead.
-		//Holds candidate values for a specific index in a board.
-		List<Integer> candidates = new ArrayList<>(9);
-		
-		Queue<Board> q = new ArrayDeque<>();
-		q.offer(new Board(board));
-
-		while (!q.isEmpty()) {
-			Board b = q.poll();
-			reduce(b);
-			
-			boolean isValid = b.isValid();
-			boolean isFull = b.isFull();
-			
-			if (isValid && isFull) {
-				return b;
-				
-			} else if (isValid) {
-				//find empty cell with least amount of options
-				int index = -1; //index of cell
-				int digits = 9; //number of digits options for cell at index
-				for (int i = 0; i < NUM_CELLS; i++) {
-					int opts = Integer.bitCount(b.getMaskAt(i));
-					if (opts > 1 && opts < digits) {
-						index = i;
-						digits = opts;
-						if (digits == 2) {
-							break;
-						}
-					}
-				}
-				
-				if (index > -1) {
-					candidates.clear();
-					b.getCandidates(index, candidates);
-					System.out.println(candidates);
-					for (Integer option : candidates) {
-						Board bCopy = new Board(b);
-						bCopy.setValueAt(index, option);
-						q.offer(bCopy);
-					}
-				}
-			}
-		}
-		
-		return null;
+		Board[] result = new Board[1];
+		searchForSolution(board, (b) -> {
+			result[0] = b;
+			return false;
+		});
+		return result[0];
 	}
 	
 	/**
@@ -84,50 +48,16 @@ public class Solver {
 	 * @param board - the Sudoku board to work on.
 	 * @return A set containing all the solutions for the given Sudoku board.
 	 */
-	public static Set<Board> getAllSolutions(Board board) {
-		//This will be reused and repopulated by board.getCandidates(list)
-		//	to reduce overhead.
-		List<Integer> candidates = new ArrayList<>(9);
-				
+	public static Set<Board> getAllSolutions(Board board) {	
 		HashSet<Board> result = new HashSet<>();
-		Queue<Board> q = new ArrayDeque<>();
-		q.offer(new Board(board));
-		//int[] values = new int[NUM_CELLS];
-
-		while (!q.isEmpty()) {
-			Board b = q.poll();
-			reduce(b);
-			//b.getValues(values);
-			if (b.isSolved()) {
-				result.add(b);
-				
-			} else if (b.isValid()) {
-				//find empty cell with least amount of options
-				int index = -1; //index of cell
-				int digits = 9; //number of digits options for cell at index
-				for (int i = 0; i < NUM_CELLS; i++) {
-					int opts = Integer.bitCount(b.board[i]);
-					if (opts > 1 && opts < digits) {
-						index = i;
-						digits = opts;
-					}
-				}
-				
-				if (index > -1) {
-					candidates.clear();
-					b.getCandidates(index, candidates);
-					for (Integer option : candidates) {
-						Board bCopy = new Board(b);
-						bCopy.setValueAt(index, option);
-						q.offer(bCopy);
-					}
-				}
-			}
-		}
-		
+		searchForSolution(board, (b) -> {
+			//System.out.println("found solution: " + b.getSimplifiedString());
+			result.add(b);
+			return true;
+		});
 		return result;
 	}
-	
+
 	/**
 	 * Determines if the given board solves uniquely to the provided solution.
 	 * <br/>This may take a long time, and possibly never return, depending on
@@ -139,52 +69,154 @@ public class Solver {
 	 * one provided; otherwise false.
 	 */
 	public static boolean solvesUniquely(Board board, Board solution) {
+		//p.execute(b) => false if b is alternate solution.
+		//When p.execute == false, search stops and returns false.
+		//When p.execute == true, search continues.
+		//Search returns true when the search is exhausted.
+		AtomicBoolean result = new AtomicBoolean(false);
+		searchForSolution(board, (b) -> {
+			//System.out.println(b.getSimplifiedString());
+			result.set(b.equals(solution));
+			return result.get();
+		});
+		return result.get();
+	}
+	
+	private static boolean searchForSolution(Board board, SolutionFoundPolicy p) {
 		//This will be reused and repopulated by board.getCandidates(list)
 		//	to reduce overhead.
 		List<Integer> candidates = new ArrayList<>(9);
-		boolean solved = false;
 		Queue<Board> q = new ArrayDeque<>();
 		q.offer(new Board(board));
-		//int[] values = new int[NUM_CELLS];
 
 		while (!q.isEmpty()) {
 			Board b = q.poll();
-			reduce(b); //O(1)
-			//b.getValues(values);
 
-			if (b.isSolved()) {
-				if (!b.equals(solution)) {
-					return false; //stop early if this is an alternate solution
+			/*if (!b.isValid()) {
+				System.out.print('*');
+				continue;
+			}*/
+
+			if (!b.isFull()) {
+				reduce(b);
+			}
+
+			if (b.isFull()) {
+				if (!p.execute(b)) {
+					return false;
 				}
-				solved = true;
-
-			} else if (b.isValid()) {
-				//find empty cell with least amount of options
-				int index = -1; //index of cell
-				int digits = 9; //number of digits options for cell at index
-				for (int i = 0; i < NUM_CELLS; i++) {
-					int opts = Integer.bitCount(b.getMaskAt(i));
-					if (opts > 1 && opts < digits) {
-						index = i;
-						digits = opts;
+			} else {
+				int index = pickEmptyCell(b);
+				if (index > 0) {
+					for (Board c : getCandidateBoards(b, index, candidates)) {
+						q.offer(c);
 					}
-				}
-
-				if (index > -1) {
-					candidates.clear();
-					b.getCandidates(index, candidates);
-					for (Integer option : candidates) {
-						Board bCopy = new Board(b);
-						bCopy.setValueAt(index, option);
-						q.offer(bCopy);
-					}
+					//putCellCandidatesInQueue(b, index, q, candidates);
 				}
 			}
 		}
 
-		return solved;
+		return true;
 	}
-	
+
+	private static boolean searchForSolution2(Board board, SolutionFoundPolicy p) {
+		//This will be reused and repopulated by board.getCandidates(list)
+		//	to reduce overhead.
+		List<Integer> candidates = new ArrayList<>(9);
+		Queue<Board> q = new ArrayDeque<>();
+		q.offer(new Board(board));
+
+		while (!q.isEmpty()) {
+			Board b = q.poll();
+
+			boolean isValid = b.isValid();
+			boolean isFull = b.isFull();
+
+			if (!isFull)
+				if (reduce(b))
+					isFull = b.isFull();
+
+			if (isValid && isFull) {
+				if (!p.execute(b)) {
+					return false;
+				}
+
+			} else if (isValid) {
+				int index = pickEmptyCell(b);
+				if (index > 0) {
+					putCellCandidatesInQueue(b, index, q, candidates);
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Searches for the index of the empty cell in board which has the fewest
+	 * number of candidates. Starts at the top of the board.
+	 */
+	private static int pickEmptyCell(Board b) {
+		int index = -1;
+		int digits = 10; //number of digit options for cell at index
+		for (int i = 0; i < NUM_CELLS; i++) {
+			int opts = Integer.bitCount(b.getMaskAt(i));
+			if (opts > 1 && opts < digits) {
+				index = i;
+				digits = opts;
+				if (digits == 2) //stop early. Won't find a cell with fewer candidates.
+					break;
+			}
+		}
+		return index;
+	}
+
+	/**
+	 * Picks the cell from board with the fewest number of candidates,
+	 * then fills in the candidates for that cell in copies of board.  Places
+	 * the copies into the queue.
+	 * <br/>Since this method is called frequently while searching for Sudoku
+	 * solutions, and Board.getCandidates() takes and returns a List, the caller may
+	 * pass in a list object to be used instead of this method allocating a new one
+	 * (and creating more garbage).
+	 * @param board - the Sudoku board trying to be solved.
+	 * @param queue - the queue of boards used to search for the solution(s).
+	 * @param candidates - a list of Integers used internally.
+	 * The list will be cleared and repopulated with the candidates for the
+	 * cell described above.
+	 */
+	private static void putCellCandidatesInQueue(Board board, int cellIndex,
+	Queue<Board> queue, List<Integer> candidates)
+	{
+		if (candidates == null)
+			candidates = new ArrayList<Integer>(9);
+
+		candidates.clear();
+		board.getCandidates(cellIndex, candidates);
+		for (int option : candidates) {
+			Board bCopy = new Board(board);
+			bCopy.setValueAt(cellIndex, option);
+			queue.offer(bCopy);
+		}
+	}
+
+	private static List<Board> getCandidateBoards(Board board, int cellIndex,
+	List<Integer> candidates)
+	{
+		if (candidates == null)
+			candidates = new ArrayList<Integer>(9);
+
+		candidates.clear();
+		board.getCandidates(cellIndex, candidates);
+		List<Board> result = new ArrayList<>();
+		for (int option : candidates) {
+			Board bCopy = new Board(board);
+			bCopy.setValueAt(cellIndex, option);
+			result.add(bCopy);
+		}
+		return result;
+	}
+
 	/**
 	 * Attempts to fill in the most obvious cells on the board.
 	 * <br/>It is possible that this method solves the puzzle.
@@ -195,7 +227,7 @@ public class Solver {
 	protected static boolean reduce(Board board) {
 		//reduce until we can't reduce no more
 	    boolean overallChange = false;
-	    boolean changed = true;
+	    boolean changed = false;
 	    int[] masks = board.getMasks(new int[NUM_CELLS]);
 	    
 	    //Track positions that are not already reduced.
@@ -206,9 +238,9 @@ public class Solver {
 	    		masks[i] = ALL;
 	    	}
 	    }
-	    
-	    while (changed) {
-	        changed = false;
+		
+		do {
+			changed = false;
 	        for (int i = indices.size() - 1; i >= 0; i--) {
 	        	int j = indices.get(i);
 	        	if (reduce(masks, j)) {
@@ -220,45 +252,34 @@ public class Solver {
 		    		indices.remove(i);
 		    	}
 	        }
-	    }
+		} while (changed);
 	    
 	    return overallChange;
 	}
 	
 	private static boolean reduce(int[] masks, int index) {
-		
 	    int initial = masks[index];
 	    
-	    //check if this cell is already reduced to one number
-	    //(if it is a power of 2)
-	    if (decode(initial) > 0) {
+	    //check if this cell already contains a single digit
+	    if (decode(initial) > 0)
 	        return false;
-	    }
 	    
 	    int candidates = ALL;
-	    
-	    //look in row
 	    candidates = reduceRow(masks, index, candidates);
-	    
-	    //look in column
 	    candidates = reduceCol(masks, index, candidates);
-	    
-	    //look region-wise
 	    candidates = reduceRegion(masks, index, candidates);
-	    
 	    masks[index] = candidates;
 	    return (initial - candidates) > 0;
 	}
 
 	private static int reduceRegion(int[] masks, int index, int candidates) {
-		//determine which grid section we are in
-	    int gr = (index / 9) / 3;
-	    int gc = (index % 9) / 3;
-	    for (int i = 0; i < 9; i++) { //i = offset from
-	    	int bi = gr*27 + gc*3 + (i / 3)*9 + (i%3);
-	        if (bi == index) {
-	            continue;
-	        }
+	    int gr = (index / 9) / 3; //grid row [0,2]
+	    int gc = (index % 9) / 3; //grid column [0,2]
+	    for (int i = 0; i < 9; i++) { //i = index of cells in region
+	    	int bi = gr*27 + gc*3 + (i / 3)*9 + (i%3); //index of region cell i in masks
+	        if (bi == index)
+				continue;
+				
 	    	int v = masks[bi];
 	        if (decode(v) > 0) {
 	            if ((candidates ^ v) < candidates) {
@@ -272,9 +293,9 @@ public class Solver {
 	private static int reduceCol(int[] masks, int index, int candidates) {
 		int c = index % 9;
 	    for (int i = c; i < 81; i += 9) {
-	        if (i == index) {
-	            continue;
-	        }
+	        if (i == index)
+				continue;
+				
 	        int v = masks[i];
 	        if (decode(v) > 0) {
 	            if ((candidates ^ v) < candidates) {
@@ -285,15 +306,11 @@ public class Solver {
 		return candidates;
 	}
 
-	//Attempt to reduce the value of 
 	private static int reduceRow(int[] masks, int index, int candidates) {
 		int r = index / 9;
 		for (int i = r * 9; i < (r + 1) * 9; i++) {
-			
-			//ignore self
-	        if (i == index) {
+	        if (i == index)
 	            continue;
-	        }
 	        
 	        int v = masks[i];
 	        if (decode(v) > 0) {
